e0c02ffc5c68b1e1791bef92222dbe461a763359
Otto Winter <otto@otto-winter.com>
2018-12-18 14:54:09 +0100

    Bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """aioesphomeapi setup script."""
 from setuptools import find_packages, setup
 
-VERSION = '1.2.0'
+VERSION = '1.3.0'
 PROJECT_NAME = 'aioesphomeapi'
 PROJECT_PACKAGE_NAME = 'aioesphomeapi'
 PROJECT_LICENSE = 'MIT'
