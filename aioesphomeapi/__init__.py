ac375d6eb7b401bab97ce843a2c39c0c2f176744
Otto winter <otto@otto-winter.com>
2021-09-14 12:41:36 +0200

    Import in init

--- aioesphomeapi/__init__.py
+++ aioesphomeapi/__init__.py
@@ -1,6 +1,16 @@
 # flake8: noqa
 from .client import APIClient
 from .connection import APIConnection, ConnectionParams
-from .core import MESSAGE_TYPE_TO_PROTO, APIConnectionError
+from .core import (
+    MESSAGE_TYPE_TO_PROTO,
+    APIConnectionError,
+    HandshakeAPIError,
+    InvalidAuthAPIError,
+    InvalidEncryptionKeyAPIError,
+    ProtocolAPIError,
+    RequiresEncryptionAPIError,
+    ResolveAPIError,
+    SocketAPIError,
+)
 from .model import *
 from .reconnect_logic import ReconnectLogic
