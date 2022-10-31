11381b0887cdeb5ec846c686183dc3a297d430de
J. Nick Koston <nick@koston.org>
2022-10-30 19:34:40 -0500

    Add a mapping for ESP_GATT_ERROR

--- aioesphomeapi/ble_defs.py
+++ aioesphomeapi/ble_defs.py
@@ -13,6 +13,7 @@ class BLEConnectionError(IntEnum):
     ESP_GATT_CONN_TERMINATE_LOCAL_HOST = 0x16
     ESP_GATT_CONN_FAIL_ESTABLISH = 0x3E
     ESP_GATT_CONN_LMP_TIMEOUT = 0x22
+    ESP_GATT_ERROR = 0x85
     ESP_GATT_CONN_CONN_CANCEL = 0x0100
     ESP_GATT_CONN_NONE = 0x0101
 
@@ -25,6 +26,7 @@ ESP_CONNECTION_ERROR_DESCRIPTION = {
     BLEConnectionError.ESP_GATT_CONN_TERMINATE_LOCAL_HOST: "Connection terminated by local host",
     BLEConnectionError.ESP_GATT_CONN_FAIL_ESTABLISH: "Connection failed to establish",
     BLEConnectionError.ESP_GATT_CONN_LMP_TIMEOUT: "Connection failed due to LMP response timeout",
+    BLEConnectionError.ESP_GATT_ERROR: "Connection failed due to GATT operation error",
     BLEConnectionError.ESP_GATT_CONN_CONN_CANCEL: "Connection cancelled",
     BLEConnectionError.ESP_GATT_CONN_NONE: "No connection to cancel",
 }
