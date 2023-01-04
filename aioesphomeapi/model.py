da77517110b83242c5d73b9af4972c953553f541
J. Nick Koston <nick@koston.org>
2023-01-03 21:45:26 -1000

    tweak

--- aioesphomeapi/model.py
+++ aioesphomeapi/model.py
@@ -837,9 +837,7 @@ class BluetoothLEAdvertisement(APIModelBase):
     manufacturer_data: Dict[int, bytes]
 
     @classmethod
-    def from_pb(  # type: ignore[misc]
-        cls: "BluetoothLEAdvertisement", data: BluetoothLEAdvertisementResponse
-    ) -> "BluetoothLEAdvertisement":
+    def from_pb(cls: Type[_V], data: Any) -> _V:
         return cls(  # type: ignore[operator, no-any-return]
             address=data.address,
             rssi=data.rssi,
