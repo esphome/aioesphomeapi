52b340f73a4d0a0d4c2f2576524aebfdf3ad222e
J. Nick Koston <nick@koston.org>
2023-05-07 08:50:37 -0500

    rework #391

--- aioesphomeapi/client.py
+++ aioesphomeapi/client.py
@@ -597,7 +597,7 @@ class APIClient:
                     f"after {timeout}s, disconnect timed out: {disconnect_timed_out}, "
                     f" after {disconnect_timeout}s"
                 ) from err
-        finally:
+        except asyncio.CancelledError:
             try:
                 unsub()
             except (KeyError, ValueError):
@@ -605,6 +605,7 @@ class APIClient:
                     "%s: Bluetooth device connection canceled but already unsubscribed",
                     addr,
                 )
+            raise
 
         return unsub
 
