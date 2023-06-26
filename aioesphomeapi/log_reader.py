f18e2c056c4bb48a22de3b67b42b33043d1485ca
J. Nick Koston <nick@koston.org>
2023-06-25 21:40:29 -0500

    cleanup

--- aioesphomeapi/log_reader.py
+++ aioesphomeapi/log_reader.py
@@ -59,7 +59,7 @@ async def main(argv: List[str]) -> None:
         except APIConnectionError:
             await cli.disconnect()
 
-    async def on_disconnect() -> None:
+    async def on_disconnect(expected_disconnect: bool) -> None:
         _LOGGER.warning("Disconnected from API")
 
     logic = ReconnectLogic(
