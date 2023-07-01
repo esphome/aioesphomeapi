fec3c3748fc98d64fdb811a7cac63b99da1f577c
J. Nick Koston <nick@koston.org>
2023-07-01 17:42:27 -0500

    force bytes

--- aioesphomeapi/_frame_helper.py
+++ aioesphomeapi/_frame_helper.py
@@ -157,6 +157,7 @@ class APIPlaintextFrameHelper(APIFrameHelper):
             # to avoid multiple calls to readexactly
             init_bytes = self._init_read(3)
             msg_type_int: Optional[int] = None
+            length_int: Optional[int] = None
             if TYPE_CHECKING:
                 assert init_bytes is not None, "Buffer should have at least 3 bytes"
             preamble, length_high, maybe_msg_type = init_bytes
