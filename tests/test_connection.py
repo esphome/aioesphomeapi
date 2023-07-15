3372b34c929c54272726212b28146417037431e5
J. Nick Koston <nick@koston.org>
2023-07-15 08:47:25 -1000

    fix tests

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -54,7 +54,7 @@ def socket_socket():
 
 def _get_mock_protocol(conn: APIConnection):
     protocol = APIPlaintextFrameHelper(
-        on_pkt=conn._process_packet,
+        on_pkt=conn._process_packet_factory(),
         on_error=conn._report_fatal_error,
         client_info="mock",
     )
