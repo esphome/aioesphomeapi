bd7dff08b4e3a12f6e3c7f003fd490f5a71afffd
J. Nick Koston <nick@koston.org>
2023-08-15 23:36:30 -0500

    Cache uuid conversions
    These get a bit expensive at startup when the services are downloaded
    from each BLE device

--- aioesphomeapi/model.py
+++ aioesphomeapi/model.py
@@ -851,6 +851,7 @@ def _join_split_uuid(value: list[int]) -> str:
     """Convert a high/low uuid into a single string."""
     return _join_split_uuid_high_low(value[0], value[1])
 
+
 @lru_cache(maxsize=256)
 def _join_split_uuid_high_low(high: int, low: int) -> str:
     return str(UUID(int=(high << 64) | low))
