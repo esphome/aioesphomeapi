9347cca3e330ab638e7b58ad4b5d5219af95c6c5
J. Nick Koston <nick@koston.org>
2023-09-08 13:49:39 -0500

    Use a memory view for zerocopy

--- aioesphomeapi/_frame_helper/noise.py
+++ aioesphomeapi/_frame_helper/noise.py
@@ -333,7 +333,7 @@ class APINoiseFrameHelper(APIFrameHelper):
         if TYPE_CHECKING:
             assert self._decrypt is not None, "Handshake should be complete"
         try:
-            msg = self._decrypt(frame)
+            msg = self._decrypt(bytes(frame))
         except InvalidTag as ex:
             self._handle_error_and_close(
                 ProtocolAPIError(f"{self._log_name}: Bad encryption frame: {ex!r}")
