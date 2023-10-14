58f56cfa503afc0bee55aa7107aa3c3674b9a851
J. Nick Koston <nick@koston.org>
2023-10-14 13:12:03 -1000

    refactor tests

--- aioesphomeapi/reconnect_logic.py
+++ aioesphomeapi/reconnect_logic.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 import asyncio
 import logging
 from collections.abc import Awaitable
-from typing import Callable
 from enum import Enum
+from typing import Callable
+
 import zeroconf
 
 from .client import APIClient
