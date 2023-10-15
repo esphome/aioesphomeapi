b877a0ef8c4250e8f168dc3a6fea14442b127654
J. Nick Koston <nick@koston.org>
2023-10-15 13:22:12 -1000

    Fix imports in host resolver test (#584)

--- tests/test_host_resolver.py
+++ tests/test_host_resolver.py
@@ -1,10 +1,11 @@
 import socket
 from ipaddress import ip_address
-import asyncio
+
 import pytest
 from mock import AsyncMock, MagicMock, patch
 from zeroconf import DNSCache
-from zeroconf.asyncio import AsyncZeroconf, AsyncServiceInfo
+from zeroconf.asyncio import AsyncServiceInfo, AsyncZeroconf
+
 import aioesphomeapi.host_resolver as hr
 from aioesphomeapi.core import APIConnectionError
 
