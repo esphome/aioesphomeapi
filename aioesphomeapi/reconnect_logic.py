f4d297a2b4210899e9172700cd6f9cbfe2f30385
J. Nick Koston <nick@koston.org>
2023-11-06 16:34:48 -0600

    Speed up stopping the reconnect logic if we are at a safe place to cancel
    If the connection has not established yet, we can cancel the
    connection process when the reconnect logic is stopped instead
    of waiting for the connection to fail. If we have already
    finished connecting and started the handshake, we wait
    for it to complete so we can try to disconnect cleanly

--- aioesphomeapi/reconnect_logic.py
+++ aioesphomeapi/reconnect_logic.py
@@ -317,6 +317,13 @@ class ReconnectLogic(zeroconf.RecordUpdateListener):
     async def stop(self) -> None:
         """Stop the connecting logic background task. Does not disconnect the client."""
         self._cancel_connect("Stopping")
+        if self._connect_task and self._connection_state == ReconnectLogicState.CONNECTING:
+            # If we are still establishing a connection, we can safely
+            # cancel the connect task here, otherwise we need to wait
+            # for the connect task to finish so we can gracefully
+            # disconnect.
+            self._connect_task.cancel("Stopping")
+
         async with self._connected_lock:
             self._is_stopped = True
             # Cancel again while holding the lock
