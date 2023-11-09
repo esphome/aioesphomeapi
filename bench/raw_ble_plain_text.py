61ceb941f7dde694d69deba5bc5e074972a3614f
J. Nick Koston <nick@koston.org>
2023-11-09 09:26:56 -0600

    Add benchmark for processing raw ble messages

--- bench/raw_ble_plain_text.py
+++ bench/raw_ble_plain_text.py
@@ -1,12 +1,12 @@
 import io
 import timeit
 
-from aioesphomeapi.api_pb2 import (
-    BluetoothLERawAdvertisementsResponse,
-    BluetoothLERawAdvertisement,
-)
 from aioesphomeapi._frame_helper import APIPlaintextFrameHelper
 from aioesphomeapi._frame_helper.plain_text import _cached_varuint_to_bytes
+from aioesphomeapi.api_pb2 import (
+    BluetoothLERawAdvertisement,
+    BluetoothLERawAdvertisementsResponse,
+)
 
 # cythonize -X language_level=3 -a -i aioesphomeapi/_frame_helper/plain_text.py
 # cythonize -X language_level=3 -a -i aioesphomeapi/_frame_helper/base.py
@@ -16,7 +16,11 @@ fake_adv = BluetoothLERawAdvertisement(
     address=1,
     rssi=-86,
     address_type=2,
-    data=b"6c04010134000000e25389019500000001016f00250000002f6f72672f626c75657a2f686369302f64656c04010134000000e25389019500000001016f00250000002f6f72672f626c75657a2f686369302f6465",
+    data=(
+        b"6c04010134000000e25389019500000001016f00250000002f6f72672f626c75"
+        b"657a2f686369302f64656c04010134000000e25389019500000001016f002500"
+        b"00002f6f72672f626c75657a2f686369302f6465"
+    ),
 )
 for i in range(5):
     adv.advertisements.append(fake_adv)
