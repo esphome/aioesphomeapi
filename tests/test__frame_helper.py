97c99e1450030f59639894c08b3877341385680d
J. Nick Koston <nick@koston.org>
2023-11-21 14:33:33 +0100

    add more coverage

--- tests/test__frame_helper.py
+++ tests/test__frame_helper.py
@@ -593,3 +593,26 @@ async def test_init_plaintext_with_wrong_preamble(conn: APIConnection):
 
     with pytest.raises(ProtocolAPIError):
         await task
+
+@pytest.mark.asyncio
+async def test_eof_received_closes_connection(
+    plaintext_connect_task_with_login: tuple[
+        APIConnection, asyncio.Transport, APIPlaintextFrameHelper, asyncio.Task
+    ],
+) -> None:
+    conn, transport, protocol, connect_task = plaintext_connect_task_with_login
+    assert protocol.eof_received() is False
+    assert conn.is_connected is False
+
+
+@pytest.mark.asyncio
+async def test_connection_lost_closes_connection_and_logs(
+    caplog: pytest.LogCaptureFixture,
+    plaintext_connect_task_with_login: tuple[
+        APIConnection, asyncio.Transport, APIPlaintextFrameHelper, asyncio.Task
+    ],
+) -> None:
+    conn, transport, protocol, connect_task = plaintext_connect_task_with_login
+    protocol.connection_lost(OSError("original message"))
+    assert conn.is_connected is False
+    assert "original message" in caplog.text
