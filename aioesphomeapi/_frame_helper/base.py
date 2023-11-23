bea28488b60743a0b76d70d422482b2594083b26
J. Nick Koston <nick@koston.org>
2023-11-23 12:06:47 -0600

    preen

--- aioesphomeapi/_frame_helper/base.py
+++ aioesphomeapi/_frame_helper/base.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import asyncio
 import logging
 from abc import abstractmethod
-from functools import partial
 from typing import TYPE_CHECKING, Callable, cast
 
 from ..core import HandshakeAPIError, SocketClosedAPIError
