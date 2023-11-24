0b758e528bfb29e4075620bcca22e229574e8d91
J. Nick Koston <nick@koston.org>
2023-11-24 08:24:23 -0600

    useless delegation

--- aioesphomeapi/_frame_helper/noise.py
+++ aioesphomeapi/_frame_helper/noise.py
@@ -110,9 +110,6 @@ class APINoiseFrameHelper(APIFrameHelper):
         self._state = NOISE_STATE_CLOSED
         super().close()
 
-    def _handle_error_and_close(self, exc: Exception) -> None:
-        super()._handle_error_and_close(exc)
-
     def _handle_error(self, exc: Exception) -> None:
         """Handle an error, and provide a good message when during hello."""
         if self._state == NOISE_STATE_HELLO and isinstance(exc, ConnectionResetError):
