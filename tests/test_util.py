ad8075dd070de9667a8c32f51775a047d1bea8b5
J. Nick Koston <nick@koston.org>
2024-08-10 21:22:23 -0500

    tweak

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,6 @@
 import asyncio
 import math
+import sys
 
 import pytest
 
@@ -32,6 +33,7 @@ def test_fix_float_single_double_conversion_nan():
     assert math.isnan(util.fix_float_single_double_conversion(float("nan")))
 
 
+@pytest.mark.skipif(sys.version_info < (3, 12), reason="Test requires Python 3.12+")
 async def test_create_eager_task_312() -> None:
     """Test create_eager_task schedules a task eagerly in the event loop.
 
@@ -49,6 +51,31 @@ async def test_create_eager_task_312() -> None:
     task2 = asyncio.create_task(_normal_task())
 
     assert events == ["eager"]
+
+    await asyncio.sleep(0)
+    assert events == ["eager", "normal"]
+    await task1
+    await task2
+
+
+@pytest.mark.skipif(sys.version_info >= (3, 12), reason="Test requires < Python 3.12")
+async def test_create_eager_task_pre_312() -> None:
+    """Test create_eager_task schedules a task in the event loop.
+
+    For older python versions, the task is scheduled normally.
+    """
+    events = []
+
+    async def _normal_task():
+        events.append("normal")
+
+    async def _eager_task():
+        events.append("eager")
+
+    task1 = util.create_eager_task(_eager_task())
+    task2 = asyncio.create_task(_normal_task())
+
+    assert events == []
 
     await asyncio.sleep(0)
     assert events == ["eager", "normal"]
