805213264dde8d37eacbcff2cd6e89564337473d
J. Nick Koston <nick@koston.org>
2024-08-29 10:26:59 -1000

    Revert "fix crash on debug logging in windows"
    This reverts commit e33ed155533d65c5c74147375b378e9b9daa282a.

--- aioesphomeapi/connection.py
+++ aioesphomeapi/connection.py
@@ -711,7 +711,9 @@ class APIConnection:
 
         if debug_enabled := self._debug_enabled:
             for msg in msgs:
-                _LOGGER.debug("%s: Sending %s", self.log_name, type(msg).__name__)
+                _LOGGER.debug(
+                    "%s: Sending %s: %s", self.log_name, type(msg).__name__, msg
+                )
 
         if TYPE_CHECKING:
             assert self._frame_helper is not None
