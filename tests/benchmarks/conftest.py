f8ac364e592a5174132f05d36b51da679382ac52
J. Nick Koston <nick@koston.org>
2025-02-02 15:24:06 -0600

    restore logger level after test

--- tests/benchmarks/conftest.py
+++ tests/benchmarks/conftest.py
@@ -6,4 +6,8 @@ import pytest
 @pytest.fixture(autouse=True)
 def no_debug_logging():
     # Enable debug logging is not on for benchmarks
-    logging.getLogger("aioesphomeapi").setLevel(logging.WARNING)
+    aioesphomeapi_logger = logging.getLogger("aioesphomeapi")
+    original_level = aioesphomeapi_logger.level
+    aioesphomeapi_logger.setLevel(logging.WARNING)
+    yield
+    aioesphomeapi_logger.setLevel(original_level)
