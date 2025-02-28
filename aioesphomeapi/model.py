c331d50177ffb1533d1f988c5cd47290fe7f048c
rwrozelle <rwrozelle@gmail.com>
2025-02-28 10:18:39 -0500

    media_player_all_features

--- aioesphomeapi/model.py
+++ aioesphomeapi/model.py
@@ -811,6 +811,9 @@ class MediaPlayerState(APIIntEnum):
     IDLE = 1
     PLAYING = 2
     PAUSED = 3
+    ANNOUNCING = 4
+    OFF = 5
+    ON = 6
 
 
 class MediaPlayerCommand(APIIntEnum):
@@ -819,7 +822,22 @@ class MediaPlayerCommand(APIIntEnum):
     STOP = 2
     MUTE = 3
     UNMUTE = 4
-
+    TOGGLE = 5
+    VOLUME_UP = 6
+    VOLUME_DOWN = 7    
+    ENQUEUE = 8,
+    REPEAT_ONE = 9,
+    REPEAT_OFF = 10,
+    CLEAR_PLAYLIST = 11,
+    NEXT_TRACK = 12,
+    PREVIOUS_TRACK = 13,
+    TURN_ON = 14,
+    TURN_OFF = 15,
+    SHUFFLE = 16,
+    UNSHUFFLE = 17,
+    REPEAT_ALL = 18,
+    JOIN = 19,
+    UNJOIN = 20,
 
 class MediaPlayerFormatPurpose(APIIntEnum):
     DEFAULT = 0
@@ -851,6 +869,9 @@ class MediaPlayerSupportedFormat(APIModelBase):
 @_frozen_dataclass_decorator
 class MediaPlayerInfo(EntityInfo):
     supports_pause: bool = False
+    supports_next_previous_track: bool = False
+    supports_turn_off_on: bool = False
+    supports_grouping: bool = False
 
     supported_formats: list[MediaPlayerSupportedFormat] = converter_field(
         default_factory=list, converter=MediaPlayerSupportedFormat.convert_list
@@ -866,6 +887,15 @@ class MediaPlayerEntityState(EntityState):
         default=0.0, converter=fix_float_single_double_conversion
     )
     muted: bool = False
+    repeat: str = ""
+    shuffle: bool = False
+
+    artist: str = ""
+    album: str = ""
+    title: str = ""
+    duration: int = 0
+    position: int = 0
+    thumbnail_url: str = ""
 
 
 # ==================== ALARM CONTROL PANEL ====================
