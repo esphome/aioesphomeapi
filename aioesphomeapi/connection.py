86e1f69e5b9bd7636e0b87ad88f1c646abf23dd4
J. Nick Koston <nick@koston.org>
2025-03-07 22:40:45 -1000

    cleanup

--- aioesphomeapi/connection.py
+++ aioesphomeapi/connection.py
@@ -882,8 +882,7 @@ class APIConnection:
             # MESSAGE_NUMBER_TO_PROTO is 0-indexed
             # but the message type is 1-indexed
             klass_merge = MESSAGE_NUMBER_TO_PROTO[msg_type_proto - 1]
-            klass = klass_merge[0]
-            merge = klass_merge[1]
+            klass, merge = klass_merge
             msg = klass()
             merge(msg, data)
         except Exception as e:
