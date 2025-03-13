43564dbccb43da199cedfa07741f19fddb6d704c
J. Nick Koston <nick@koston.org>
2025-03-12 14:05:09 -1000

    adjust comment

--- aioesphomeapi/connection.py
+++ aioesphomeapi/connection.py
@@ -940,7 +940,7 @@ class APIConnection:
                 handler(msg)
             return
 
-        # Most common cast, only one handler
+        # Most common case, only one handler
         # no need to copy the set. We still
         # use a loop here even though there is
         # only one handler because Cython will
