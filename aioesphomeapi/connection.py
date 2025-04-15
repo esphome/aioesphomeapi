b228c8243efcd9745208239fb31f76cae9c9b441
J. Nick Koston <nick@koston.org>
2025-04-15 13:31:58 -1000

    Avoid protobuf lookup to create messages
    This is effectively the same thing as #1100 but for writing
    instead of reading

--- aioesphomeapi/connection.py
+++ aioesphomeapi/connection.py
@@ -71,7 +71,9 @@ PING_REQUEST_MESSAGES = (PingRequest(),)
 PING_RESPONSE_MESSAGES = (PingResponse(),)
 NO_PASSWORD_CONNECT_REQUEST = ConnectRequest()
 
-PROTO_TO_MESSAGE_TYPE = {v: k for k, v in MESSAGE_TYPE_TO_PROTO.items()}
+PROTO_TO_MESSAGE_TYPE: dict[
+    type[message.Message], tuple[int, Callable[[message.Message], bytes]]
+] = {v: (k, v.SerializeToString) for k, v in MESSAGE_TYPE_TO_PROTO.items()}
 
 KEEP_ALIVE_TIMEOUT_RATIO = 4.5
 #
@@ -707,9 +709,10 @@ class APIConnection:
             )
 
         packets: list[tuple[int, bytes]] = [
-            (PROTO_TO_MESSAGE_TYPE[type(msg)], msg.SerializeToString()) for msg in msgs
+            (msg_type[0], msg_type[1](msg))
+            for msg in msgs
+            if (msg_type := PROTO_TO_MESSAGE_TYPE[type(msg)])
         ]
-
         if debug_enabled := self._debug_enabled:
             for msg in msgs:
                 _LOGGER.debug(
