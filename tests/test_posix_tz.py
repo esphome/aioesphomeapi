44381f2cbc5313c992b6c6daea0f4b500639c605
J. Nick Koston <nick@home-assistant.io>
2026-02-23 13:32:45 -0600

    proto update

--- tests/test_posix_tz.py
+++ tests/test_posix_tz.py
@@ -16,31 +16,31 @@ from aioesphomeapi.posix_tz import DSTRuleType, ParsedTimezone, parse_posix_tz
 
 
 def test_parse_simple_offset_est5() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5")
+    tz = parse_posix_tz("EST5")
     assert tz.std_offset_seconds == 5 * 3600  # +5 hours (west of UTC)
     assert not tz.has_dst
 
 
 def test_parse_negative_offset_cet() -> None:
-    tz: ParsedTimezone = parse_posix_tz("CET-1")
+    tz = parse_posix_tz("CET-1")
     assert tz.std_offset_seconds == -1 * 3600  # -1 hour (east of UTC)
     assert not tz.has_dst
 
 
 def test_parse_explicit_positive_offset() -> None:
-    tz: ParsedTimezone = parse_posix_tz("TEST+5")
+    tz = parse_posix_tz("TEST+5")
     assert tz.std_offset_seconds == 5 * 3600
     assert not tz.has_dst
 
 
 def test_parse_zero_offset() -> None:
-    tz: ParsedTimezone = parse_posix_tz("UTC0")
+    tz = parse_posix_tz("UTC0")
     assert tz.std_offset_seconds == 0
     assert not tz.has_dst
 
 
 def test_parse_us_eastern_with_dst() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,M3.2.0,M11.1.0")
+    tz = parse_posix_tz("EST5EDT,M3.2.0,M11.1.0")
     assert tz.std_offset_seconds == 5 * 3600
     assert tz.dst_offset_seconds == 4 * 3600  # Default: STD - 1hr
     assert tz.has_dst
@@ -54,7 +54,7 @@ def test_parse_us_eastern_with_dst() -> None:
 
 
 def test_parse_us_central_with_time() -> None:
-    tz: ParsedTimezone = parse_posix_tz("CST6CDT,M3.2.0/2,M11.1.0/2")
+    tz = parse_posix_tz("CST6CDT,M3.2.0/2,M11.1.0/2")
     assert tz.std_offset_seconds == 6 * 3600
     assert tz.dst_offset_seconds == 5 * 3600
     assert tz.dst_start.time_seconds == 2 * 3600  # 2:00 AM
@@ -62,7 +62,7 @@ def test_parse_us_central_with_time() -> None:
 
 
 def test_parse_europe_berlin() -> None:
-    tz: ParsedTimezone = parse_posix_tz("CET-1CEST,M3.5.0,M10.5.0/3")
+    tz = parse_posix_tz("CET-1CEST,M3.5.0,M10.5.0/3")
     assert tz.std_offset_seconds == -1 * 3600
     assert tz.dst_offset_seconds == -2 * 3600  # Default: STD - 1hr
     assert tz.has_dst
@@ -74,7 +74,7 @@ def test_parse_europe_berlin() -> None:
 
 
 def test_parse_new_zealand() -> None:
-    tz: ParsedTimezone = parse_posix_tz("NZST-12NZDT,M9.5.0,M4.1.0/3")
+    tz = parse_posix_tz("NZST-12NZDT,M9.5.0,M4.1.0/3")
     assert tz.std_offset_seconds == -12 * 3600
     assert tz.dst_offset_seconds == -13 * 3600  # Default: STD - 1hr
     assert tz.has_dst
@@ -83,7 +83,7 @@ def test_parse_new_zealand() -> None:
 
 
 def test_parse_explicit_dst_offset() -> None:
-    tz: ParsedTimezone = parse_posix_tz("TEST5DST4,M3.2.0,M11.1.0")
+    tz = parse_posix_tz("TEST5DST4,M3.2.0,M11.1.0")
     assert tz.std_offset_seconds == 5 * 3600
     assert tz.dst_offset_seconds == 4 * 3600
     assert tz.has_dst
@@ -95,19 +95,19 @@ def test_parse_explicit_dst_offset() -> None:
 
 
 def test_parse_angle_bracket_positive() -> None:
-    tz: ParsedTimezone = parse_posix_tz("<+07>-7")
+    tz = parse_posix_tz("<+07>-7")
     assert tz.std_offset_seconds == -7 * 3600  # -7 = 7 hours east of UTC
     assert not tz.has_dst
 
 
 def test_parse_angle_bracket_negative() -> None:
-    tz: ParsedTimezone = parse_posix_tz("<-03>3")
+    tz = parse_posix_tz("<-03>3")
     assert tz.std_offset_seconds == 3 * 3600
     assert not tz.has_dst
 
 
 def test_parse_angle_bracket_with_dst() -> None:
-    tz: ParsedTimezone = parse_posix_tz("<+10>-10<+11>,M10.1.0,M4.1.0/3")
+    tz = parse_posix_tz("<+10>-10<+11>,M10.1.0,M4.1.0/3")
     assert tz.std_offset_seconds == -10 * 3600
     assert tz.dst_offset_seconds == -11 * 3600
     assert tz.has_dst
@@ -116,13 +116,13 @@ def test_parse_angle_bracket_with_dst() -> None:
 
 
 def test_parse_angle_bracket_named() -> None:
-    tz: ParsedTimezone = parse_posix_tz("<AEST>-10")
+    tz = parse_posix_tz("<AEST>-10")
     assert tz.std_offset_seconds == -10 * 3600
     assert not tz.has_dst
 
 
 def test_parse_angle_bracket_with_minutes() -> None:
-    tz: ParsedTimezone = parse_posix_tz("<+0545>-5:45")
+    tz = parse_posix_tz("<+0545>-5:45")
     assert tz.std_offset_seconds == -(5 * 3600 + 45 * 60)
     assert not tz.has_dst
 
@@ -133,36 +133,36 @@ def test_parse_angle_bracket_with_minutes() -> None:
 
 
 def test_parse_offset_india() -> None:
-    tz: ParsedTimezone = parse_posix_tz("IST-5:30")
+    tz = parse_posix_tz("IST-5:30")
     assert tz.std_offset_seconds == -(5 * 3600 + 30 * 60)
     assert not tz.has_dst
 
 
 def test_parse_offset_nepal() -> None:
-    tz: ParsedTimezone = parse_posix_tz("NPT-5:45")
+    tz = parse_posix_tz("NPT-5:45")
     assert tz.std_offset_seconds == -(5 * 3600 + 45 * 60)
     assert not tz.has_dst
 
 
 def test_parse_offset_with_seconds() -> None:
-    tz: ParsedTimezone = parse_posix_tz("TEST-1:30:30")
+    tz = parse_posix_tz("TEST-1:30:30")
     assert tz.std_offset_seconds == -(1 * 3600 + 30 * 60 + 30)
 
 
 def test_parse_chatham_islands() -> None:
-    tz: ParsedTimezone = parse_posix_tz("<+1245>-12:45<+1345>,M9.5.0/2:45,M4.1.0/3:45")
+    tz = parse_posix_tz("<+1245>-12:45<+1345>,M9.5.0/2:45,M4.1.0/3:45")
     assert tz.std_offset_seconds == -(12 * 3600 + 45 * 60)
     assert tz.dst_offset_seconds == -(13 * 3600 + 45 * 60)
     assert tz.has_dst
 
 
 def test_parse_max_offset_14_hours() -> None:
-    tz: ParsedTimezone = parse_posix_tz("<+14>-14")
+    tz = parse_posix_tz("<+14>-14")
     assert tz.std_offset_seconds == -14 * 3600
 
 
 def test_parse_max_negative_offset_12_hours() -> None:
-    tz: ParsedTimezone = parse_posix_tz("<-12>12")
+    tz = parse_posix_tz("<-12>12")
     assert tz.std_offset_seconds == 12 * 3600
 
 
@@ -202,7 +202,7 @@ def test_parse_comma_without_dst_name_fails() -> None:
 
 
 def test_parse_j_format_basic() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,J60,J305")
+    tz = parse_posix_tz("EST5EDT,J60,J305")
     assert tz.has_dst
     assert tz.dst_start.type == DSTRuleType.JULIAN_NO_LEAP
     assert tz.dst_start.day == 60
@@ -211,7 +211,7 @@ def test_parse_j_format_basic() -> None:
 
 
 def test_parse_j_format_with_time() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,J60/2,J305/2")
+    tz = parse_posix_tz("EST5EDT,J60/2,J305/2")
     assert tz.dst_start.day == 60
     assert tz.dst_start.time_seconds == 2 * 3600
     assert tz.dst_end.day == 305
@@ -219,7 +219,7 @@ def test_parse_j_format_with_time() -> None:
 
 
 def test_parse_plain_day_number() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,59,304")
+    tz = parse_posix_tz("EST5EDT,59,304")
     assert tz.has_dst
     assert tz.dst_start.type == DSTRuleType.DAY_OF_YEAR
     assert tz.dst_start.day == 59
@@ -238,7 +238,7 @@ def test_parse_j_format_invalid_day_366() -> None:
 
 
 def test_parse_plain_day_with_time() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,59/3,304/1:30")
+    tz = parse_posix_tz("EST5EDT,59/3,304/1:30")
     assert tz.dst_start.day == 59
     assert tz.dst_start.time_seconds == 3 * 3600
     assert tz.dst_end.day == 304
@@ -256,28 +256,28 @@ def test_parse_plain_day_invalid_366() -> None:
 
 
 def test_parse_negative_transition_time() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,M3.2.0/-1,M11.1.0/2")
+    tz = parse_posix_tz("EST5EDT,M3.2.0/-1,M11.1.0/2")
     assert tz.dst_start.time_seconds == -1 * 3600
     assert tz.dst_end.time_seconds == 2 * 3600
 
 
 def test_parse_negative_transition_time_with_minutes() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,M3.2.0/-1:30,M11.1.0")
+    tz = parse_posix_tz("EST5EDT,M3.2.0/-1:30,M11.1.0")
     assert tz.dst_start.time_seconds == -(1 * 3600 + 30 * 60)
 
 
 def test_parse_large_transition_time() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,M3.2.0/25,M11.1.0")
+    tz = parse_posix_tz("EST5EDT,M3.2.0/25,M11.1.0")
     assert tz.dst_start.time_seconds == 25 * 3600
 
 
 def test_parse_max_transition_time_167_hours() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,M3.2.0/167,M11.1.0")
+    tz = parse_posix_tz("EST5EDT,M3.2.0/167,M11.1.0")
     assert tz.dst_start.time_seconds == 167 * 3600
 
 
 def test_parse_transition_time_hours_minutes_seconds() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,M3.2.0/2:30:45,M11.1.0")
+    tz = parse_posix_tz("EST5EDT,M3.2.0/2:30:45,M11.1.0")
     assert tz.dst_start.time_seconds == 2 * 3600 + 30 * 60 + 45
 
 
@@ -332,14 +332,14 @@ def test_parse_missing_end_rule_plain_day() -> None:
 
 
 def test_parse_lowercase_m_format() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,m3.2.0,m11.1.0")
+    tz = parse_posix_tz("EST5EDT,m3.2.0,m11.1.0")
     assert tz.has_dst
     assert tz.dst_start.month == 3
     assert tz.dst_end.month == 11
 
 
 def test_parse_lowercase_j_format() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,j60,j305")
+    tz = parse_posix_tz("EST5EDT,j60,j305")
     assert tz.dst_start.type == DSTRuleType.JULIAN_NO_LEAP
     assert tz.dst_start.day == 60
 
@@ -351,14 +351,14 @@ def test_parse_lowercase_j_format() -> None:
 
 def test_parse_dst_name_without_rules() -> None:
     """DST name present but no rules - treat as no DST."""
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT")
+    tz = parse_posix_tz("EST5EDT")
     assert not tz.has_dst
     assert tz.std_offset_seconds == 5 * 3600
 
 
 def test_parse_trailing_characters_ignored() -> None:
     """Trailing characters after valid TZ should be ignored."""
-    tz: ParsedTimezone = parse_posix_tz("EST5 extra garbage here")
+    tz = parse_posix_tz("EST5 extra garbage here")
     assert tz.std_offset_seconds == 5 * 3600
     assert not tz.has_dst
 
@@ -370,7 +370,7 @@ def test_parse_trailing_characters_ignored() -> None:
 
 def test_default_dst_rule_fields() -> None:
     """DSTRule with type=NONE should have zero fields."""
-    tz: ParsedTimezone = parse_posix_tz("EST5")
+    tz = parse_posix_tz("EST5")
     assert tz.dst_start.type == DSTRuleType.NONE
     assert tz.dst_start.time_seconds == 0
     assert tz.dst_start.day == 0
@@ -382,7 +382,7 @@ def test_default_dst_rule_fields() -> None:
 
 def test_default_transition_time_2am() -> None:
     """M-format without /time should default to 2:00 AM."""
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,M3.2.0,M11.1.0")
+    tz = parse_posix_tz("EST5EDT,M3.2.0,M11.1.0")
     assert tz.dst_start.time_seconds == 2 * 3600
     assert tz.dst_end.time_seconds == 2 * 3600
 
@@ -409,7 +409,7 @@ def test_parsed_timezone_default() -> None:
 
 
 def test_parse_america_new_york() -> None:
-    tz: ParsedTimezone = parse_posix_tz("EST5EDT,M3.2.0,M11.1.0")
+    tz = parse_posix_tz("EST5EDT,M3.2.0,M11.1.0")
     assert tz.std_offset_seconds == 5 * 3600
     assert tz.dst_offset_seconds == 4 * 3600
     assert tz.dst_start.type == DSTRuleType.MONTH_WEEK_DAY
@@ -421,7 +421,7 @@ def test_parse_america_new_york() -> None:
 
 
 def test_parse_europe_london() -> None:
-    tz: ParsedTimezone = parse_posix_tz("GMT0BST,M3.5.0/1,M10.5.0")
+    tz = parse_posix_tz("GMT0BST,M3.5.0/1,M10.5.0")
     assert tz.std_offset_seconds == 0
     assert tz.dst_offset_seconds == -3600
     assert tz.dst_start.month == 3
@@ -432,13 +432,13 @@ def test_parse_europe_london() -> None:
 
 
 def test_parse_asia_tokyo() -> None:
-    tz: ParsedTimezone = parse_posix_tz("JST-9")
+    tz = parse_posix_tz("JST-9")
     assert tz.std_offset_seconds == -9 * 3600
     assert not tz.has_dst
 
 
 def test_parse_australia_sydney() -> None:
-    tz: ParsedTimezone = parse_posix_tz("AEST-10AEDT,M10.1.0,M4.1.0/3")
+    tz = parse_posix_tz("AEST-10AEDT,M10.1.0,M4.1.0/3")
     assert tz.std_offset_seconds == -10 * 3600
     assert tz.dst_offset_seconds == -11 * 3600
     assert tz.dst_start.month == 10
@@ -447,7 +447,7 @@ def test_parse_australia_sydney() -> None:
 
 
 def test_parse_pacific_auckland() -> None:
-    tz: ParsedTimezone = parse_posix_tz("NZST-12NZDT,M9.5.0,M4.1.0/3")
+    tz = parse_posix_tz("NZST-12NZDT,M9.5.0,M4.1.0/3")
     assert tz.std_offset_seconds == -12 * 3600
     assert tz.dst_offset_seconds == -13 * 3600
     assert tz.dst_start.month == 9
@@ -455,12 +455,12 @@ def test_parse_pacific_auckland() -> None:
 
 
 def test_parse_america_chicago() -> None:
-    tz: ParsedTimezone = parse_posix_tz("CST6CDT,M3.2.0,M11.1.0")
+    tz = parse_posix_tz("CST6CDT,M3.2.0,M11.1.0")
     assert tz.std_offset_seconds == 6 * 3600
     assert tz.dst_offset_seconds == 5 * 3600
 
 
 def test_parse_asia_kolkata() -> None:
-    tz: ParsedTimezone = parse_posix_tz("IST-5:30")
+    tz = parse_posix_tz("IST-5:30")
     assert tz.std_offset_seconds == -(5 * 3600 + 30 * 60)
     assert not tz.has_dst
